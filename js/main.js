79d00a2ee87d24dac61f040e6c54bfcd21b94cfc
Mind <mindstrade2024@gmail.com>
2024-12-06 10:21:41 -0300

    adding firsts files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Cuando el usuario clicquee en el boton de SignUp se lo reeenviará a la main page 
+//luego los datos se subirán a un storageTemp
+// si los datos del usuario no estan completos en los campos se le pedirá que los complete
+
+
+let buckUpData = () =>{
+    
+}
