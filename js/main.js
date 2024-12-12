8b4a69dc0dd0a2f0ebb675e72a2cf9728c58259c
Mind <99829678+Julmax04@users.noreply.github.com>
2024-12-12 12:01:03 -0300

    adding the logic
    ingresando la lógica del formulario para enviar los datos del usuario al Storage y realizar una validacion

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-//1°: Extraer los datos del usuario y guardarlos en un bukup
-//2°:Cuando el usuario haga click en el botón, se enviarán los datos al Storage
-//3°:Mostramos mensaje que los datos se enviaron con exito
+// Realizar validaciones para que no haya campos vacios o sean invalidos
+//Enviar los datos al Storage del navegador
 
+
+//acceder a los datos del los usuarios
+// luego realizamos validacion
+// si pasa la validacion, procesamos los datos
+
+
+const nameI = document.querySelector("#name").value.trim()
+const passwordI = document.querySelector("#password").value.trim()
+const emailI = document.querySelector("#email").value.trim()
+
+
+function notification (text){
+    Swal.fire(text)
+}
+
+function validateInputs () {
+    const form = querySelector(".form")
+    form.addEventListener("submit", function(e){
+        //prevenimos la carga del formulario
+        e.preventDefault()
+
+   if(!nameI || !passwordI || !emailI){
+     notification("please complete all inputs")
+   }  
+
+   else {
+    notification("data validated", {nameI, passwordI, emailI})
+   }
+})
+}
+
+validateInputs()
