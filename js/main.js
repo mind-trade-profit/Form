db21f97c3917566a5f3ae0e47944cf3952d28e92
Mind <99829678+Julmax04@users.noreply.github.com>
2024-12-24 10:22:48 -0300

    Obtain and send data of user
    Añadiendo lógica para almacenar los datos del usuario en el storage y obtener en un tabla

--- js/main.js
+++ js/main.js
@@ -1,35 +1,62 @@
 // Realizar validaciones para que no haya campos vacios o sean invalidos
 //Enviar los datos al Storage del navegador
 
-
 //acceder a los datos del los usuarios
 // luego realizamos validacion
 // si pasa la validacion, procesamos los datos
 
-
-const nameI = document.querySelector("#name").value.trim()
-const passwordI = document.querySelector("#password").value.trim()
-const emailI = document.querySelector("#email").value.trim()
-
-
-function notification (text){
-    Swal.fire(text)
+function notification (text, classN, color1,color2){
+  Toastify({
+    text: text,
+    className: classN,
+    style: {
+      background: `linear-gradient(to right, ${color1},  ${color2})`,
+      color: "#111114"
+    },
+    duration: 2000
+  }).showToast()
 }
 
-function validateInputs () {
-    const form = querySelector(".form")
-    form.addEventListener("submit", function(e){
-        //prevenimos la carga del formulario
-        e.preventDefault()
 
-   if(!nameI || !passwordI || !emailI){
-     notification("please complete all inputs")
-   }  
+//base de datos de usuario
 
-   else {
-    notification("data validated", {nameI, passwordI, emailI})
-   }
+function user (name,password,email){
+
+ let dataUsers = {
+  id : Math.random(10),
+  name: name,
+  password: password,
+  email: email
+ }
+
+ JSON.stringify(localStorage.setItem(dataUsers.id, `${name},${password},${email}`))
+}
+
+function validateInputs() {
+  const form = document.querySelector(".form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nameI = document.querySelector("#nameI").value.trim()
+    const passwordI = document.querySelector("#password").value.trim()
+    const emailI = document.querySelector("#email").value.trim()
+    
+    user(nameI,passwordI,emailI)
+
+  if(nameI && passwordI && emailI) {
+    notification("The Prompts Sent", "Success","#00FF9C", "#73EC8B")
+  } 
 })
 }
-
 validateInputs()
+
+//accder a los datos de los usuarios y almacenarlos en una tabla
+
+function getDataUsers (){
+   for(let i = 0; i < localStorage.length; i++){
+    let getData = JSON.parse(localStorage.key(i))
+
+   console.table(getData, localStorage.getItem(getData))
+   } 
+
+ 
+}
